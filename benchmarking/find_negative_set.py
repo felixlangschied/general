68ab7702d48d4c41af48711bc387a2e8ab83256c
felixlangschied <felix_langschied@web.de>
2021-07-23 10:33:43 +0200

    negative set

---
+++ benchmarking/find_negative_set.py
@@ -0,0 +1,19 @@
+import os
+import subprocess as sp
+
+over05_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\05overview.txt'
+# over05_path = '/home/felixl/PycharmProjects/general/benchmarking/data/05overview.txt'
+outpath = '/home/felixl/PycharmProjects/general/benchmarking/data/RNAfold'
+if not os.path.isdir(outpath):
+    os.mkdir(outpath)
+
+with open(over05_path, 'r') as fh:
+    for line in fh:
+        ipath = line.split()[0]
+        print(ipath)
+        fname = '_'.join(ipath.split('/')[-2:]).replace('_pre_orthologs.fa', '')
+        fold_cmd = f'RNAfold -i {ipath} -o {outpath}/{fname}.txt'
+        sp.run(fold_cmd, shell=True)
+
+
+        break
