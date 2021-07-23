866ea69ea660164319a9e7514fa3b17bd2ae1352
felixlangschied <felix_langschied@web.de>
2021-07-23 13:25:12 +0200

    negative set

--- benchmarking/find_negative_set.py
+++ benchmarking/find_negative_set.py
@@ -1,9 +1,10 @@
 import os
 import subprocess as sp
+import json
 
 # over05_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\05overview.txt'
 over05_path = '/home/felixl/PycharmProjects/general/benchmarking/data/05overview.txt'
-outpath = '/home/felixl/PycharmProjects/general/benchmarking/data/RNAfold'
+outpath = '/home/felixl/PycharmProjects/general/benchmarking/data'
 if not os.path.isdir(outpath):
     os.mkdir(outpath)
 
@@ -20,12 +21,14 @@ with open(over05_path, 'r') as fh:
         fold_cmd = f'RNAfold -i {ipath}'
         res = sp.run(fold_cmd, shell=True, capture_output=True)
         out = res.stdout.decode('utf-8')
-        score = out.split(' (')[1].replace(')', '')
+        score = out.split(' (')[1].replace(')\n', '')
         # fill dict
         if specname not in outdict:
             outdict[specname] = {}
         outdict[specname][mirname] = score
         allscores.append(score)
         print(out)
-        break
-print(outdict)
+
+outfile = f'{outpath}/all05_RNAfold.json'
+with open(outfile, 'w') as of:
+    json.dump(outdict)
