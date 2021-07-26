745430da0ee8c4fc819f3323a22b4f3e26616222
felixlangschied <felix_langschied@web.de>
2021-07-26 16:51:39 +0200

    negative set

--- benchmarking/find_negative_set.py
+++ benchmarking/find_negative_set.py
@@ -8,29 +8,50 @@ outpath = '/home/felixl/PycharmProjects/general/benchmarking/data'
 if not os.path.isdir(outpath):
     os.mkdir(outpath)
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-outdict = {}
-allscores = []
-out_stdout = f'{outpath}/all05_RNAfold_results.txt'
-with open(over05_path, 'r') as fh, open(out_stdout, 'w') as of:
-    for line in fh:
+df_dict = {'species': [], 'mirna_coorth': [], 'mirna': [], 'mirna_fam': [], 'score': [], 'seq': [], 'scheme': []}
+# out_stdout = f'{outpath}/all05_RNAfold_results.txt'
+# with open(over05_path, 'r') as fh, open(out_stdout, 'w') as of:
+with open(over05_path, 'r') as fh:
+    for c, line in enumerate(fh):
+        print(f'{c} / 15880')
         ipath = line.split()[0].replace('mirGeneDB', 'mirgenedb')
         specname = ipath.split('/')[7]
-        mirname = ipath.split('/')[-1].replace('_pre_orthologs.fa', '')
-
         # run RNAfold
         fold_cmd = f'RNAfold -i {ipath}'
         res = sp.run(fold_cmd, shell=True, capture_output=True)
-        out = res.stdout.decode('utf-8')
-        of.write(out)
-        score = out.split(' (')[1].replace(')\n', '')
-        # fill dict
-        if specname not in outdict:
-            outdict[specname] = {}
-        outdict[specname][mirname] = score
-        allscores.append(score)
-        print(out)
-
+        out = res.stdout.decode('utf-8').strip()
+        data = out.split('\n')
+        data = chunks(data, 3)
+        for chunk in list(data):
+            # print(chunk)
+            header, seq, schemscore = chunk
+            mirname = header.split('|')[0].replace('>', '')
+            mirna = mirname.split('_')[0]
+            mirfam = '-'.join(mirna.split('-')[0:3])
+            # print(schemscore.split(' ('))
+            scheme, score = schemscore.split(' (')
+            score = score.replace(')', '')
+            # fill dict
+            df_dict['species'].append(specname)
+            df_dict['mirna_coorth'].append(mirname)
+            df_dict['mirna'].append(mirna)
+            df_dict['mirna_fam'].append(mirfam)
+            df_dict['score'].append(score)
+            df_dict['seq'].append(seq)
+            df_dict['scheme'].append(scheme)
+            # remove tmp file
+            cur_dir = os.getcwd()
+            to_rmv = cur_dir + '/' + header.replace('>', '').replace('|', '_') + '_ss.ps'
+            if os.path.isfile(to_rmv):
+                os.remove(to_rmv)
+        # if c == 100:
+        #     break
+# print(df_dict)
 outfile = f'{outpath}/all05_RNAfold.json'
 with open(outfile, 'w') as of:
-    json.dump(outdict, of)
+    json.dump(df_dict, of)
--- benchmarking/visualize_RNAfold.py
+++ benchmarking/visualize_RNAfold.py
@@ -1,17 +1,22 @@
 import json
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-json_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\all05_RNAfold_results.txt'
+# json_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\all05_RNAfold_results.txt'
+json_path = '/home/felixl/PycharmProjects/general/benchmarking/data/all05_RNAfold.json'
 
-all_scores = []
 with open(json_path, 'r') as fh:
-    for line in fh:
-        if line.strip().endswith(')'):
-            score = float(line.strip().split(' (')[1].replace(')', ''))
-            all_scores.append(score)
+    sdict = json.load(fh)
 
-print(sorted(all_scores, reverse=True))
+df = pd.DataFrame.from_dict(sdict)
+df['score'] = pd.to_numeric(df['score'])
+print(df.columns)
+for i in list(df.columns):
+    print(df[i].head(1))
 
-sns.histplot(all_scores)
-plt.show() m
+sns.histplot(data=df, x='score')
+plt.title('Unfiltered score distribution')
+plt.show()
+
+plt.figure()
