f62e96f22ac42c12ec6fdece4da3fc7d9d7d78f4
felixlangschied <felix_langschied@web.de>
2021-08-03 16:22:36 +0200

    mirgendb entries

--- benchmarking/data/mirgenedb/extract_vertebrates.py
+++ benchmarking/data/mirgenedb/extract_vertebrates.py
@@ -2,7 +2,8 @@
 
 all_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\mirgenedb\all_mirgenedb.fa'
 abb_path = r'C:\Users\felix\Desktop\mirgene_db.CSV'
-out_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\mirgenedb\named_all_mirgenedb.fa'
+out_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\mirgenedb\benchmark_mirgenedb.fa'
+spec_path = r'C:\Users\felix\PycharmProjects\general\benchmarking\data\spec_missing_TP.tsv'
 
 map_dict = {}
 with open(abb_path, 'r') as fh:
@@ -17,11 +18,20 @@ with open(abb_path, 'r') as fh:
             map_dict[abb] = name
 # print(map_dict)
 
+specs = []
+with open(spec_path, 'r') as fh:
+    next(fh)
+    for line in fh:
+        specs.append(line.split('\t')[0])
+
 
 with open(all_path, 'r') as fh, open(out_path, 'w') as of:
     data = fh.read().split('>')
     for entry in data:
         db_abb = entry[0:3]
-        parsed_entry = '>' + entry.replace(db_abb, map_dict[db_abb])
+        name = map_dict[db_abb]
+        if name not in specs:
+            continue
+        parsed_entry = '>' + entry.replace(db_abb, name)
         print(parsed_entry.strip())
         of.write(parsed_entry)
